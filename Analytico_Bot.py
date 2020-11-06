4d24fb1e098e8df8cd8950dc6f37194051146013
Kyle-KJ <kknightjenkins@gmail.com>
2020-11-06 13:43:26 +0000

    Expanded graphing options and help text

--- Analytico_Bot.py
+++ Analytico_Bot.py
@@ -15,7 +15,12 @@ import seaborn as sns
 def date_filename():
     # Create image filename using current datetime
     return re.sub(r'\ |\.|\:|\-', '_', str(datetime.datetime.utcnow())) + ".png"
-    
+
+
+# Configurable Parameters
+COMMAND_PREFIX = '$aco '
+POST_COLOUR = 0x03BDAB
+
 
 main_dir = os.path.dirname(__file__)
 token_path = '/Parameters/Bot_Token.txt'
@@ -23,9 +28,8 @@ token_file = main_dir + token_path
 
 bot_token = open(token_file, 'r').read()
 
-bot = commands.Bot(command_prefix='$aco ')
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
-post_colour= 0x03BDAB
 
 @bot.event
 async def on_ready():
@@ -51,9 +55,51 @@ async def test(ctx):
 
 @bot.command()
 async def helpme(ctx):
-    # TODO - Provide help text containing list of available functions
-    # TODO - Add optional extra argument to give deeper detail and examples for specific functions 
-    await ctx.channel.send("Help text.")
+
+    help_text = """```
+
+Analytico Commands: (Note - All commands must be prefixed with '{prefix}')
+
+    Graph Commands: (These commands will generate a graph image)
+
+        graph messagecount user
+
+            Returns bar chart of messages posted by user
+
+        graph messagecount channel
+
+            Returns bar chart of messages posted by channel
+
+        graph wordcount
+
+            Returns bar chart of word count posted by user 
+
+        graph wordcount <your_word>
+
+            Returns bar chart of specific word posted by user
+            Replace <your_word> with word to search
+
+        graph emojicount
+
+            Returns bar chart of count of times emoji posted
+    
+    Other Commands:
+
+        messagecount <unit> <value>
+
+            Returns count of messages posted within the specified time period
+            Replace <unit> and <value> with preferred time period
+
+            Valid for <unit>: minutes, hours, days, weeks, years
+            Valid for <value>: Any positive integer
+
+        test
+            Bot will respond if online.
+    ```"""
+
+    help_text = help_text.format(prefix=COMMAND_PREFIX)
+
+    await ctx.channel.send(help_text)
 
 
 @bot.command()
@@ -90,15 +136,19 @@ async def graph(ctx, *args):
     # TODO - Add date filters
 
     # Get Message History (as DataFrame)
-    message_list = await ctx.channel.history().flatten()
+    message_list = []
+
+    for chan in bot.guilds[0].text_channels:
+        channel_messages = await chan.history(limit=50000).flatten()
+        message_list += channel_messages
 
     messages = []
 
     for msg in message_list:
-        message_data = [msg.author.name, msg.content]
+        message_data = [msg.author.name, msg.channel.name, msg.content]
         messages.append(message_data)
     
-    df = pd.DataFrame(messages, columns=['User', 'Message'])
+    #df = pd.DataFrame(messages, columns=['User', 'Channel', 'Message'])
 
     # Assign Filename for Image
     filename = date_filename()    
@@ -106,30 +156,71 @@ async def graph(ctx, *args):
 
     if args[0] == "messagecount":
 
-        # Plot Chart and Save Image
-        sns.countplot(x='User', data=df)
-        plt.savefig(filename, dpi=200)
+        df = pd.DataFrame(messages, columns=['User', 'Channel', 'Message'])
 
-        # Construct Embedded Message
-        embed = discord.Embed(
-            title="Message Count Graph",
-            description="Number of messages posted per user",
-            colour=post_colour
-            )
+        try:
+            if args[1] == "user":
 
-        graph_file = discord.File(graph_path, filename=filename)
-        attach_path = "attachment://" + filename
-        embed.set_image(url=attach_path)
-        embed.set_footer(text="Chart generated using Seaborn")
+                # Transform Data
+                grouped = df.groupby('User').size().reset_index(name='Count')
+                
+                # Plot Chart and Save Image
+                sns.barplot(x='User', y='Count', data=grouped)
+                plt.savefig(filename, dpi=200)
+                plt.clf()
 
-        # Post Message
-        await ctx.channel.send(file=graph_file, embed=embed)
+                # Construct Embedded Message
+                embed = discord.Embed(
+                    title="Message Count Graph",
+                    description="Number of messages posted per user",
+                    colour=POST_COLOUR
+                    )
 
-        # Delete Image
-        os.remove(graph_path)
+                graph_file = discord.File(graph_path, filename=filename)
+                attach_path = "attachment://" + filename
+                embed.set_image(url=attach_path)
+                embed.set_footer(text="Chart generated using Seaborn")
+
+                # Post Message
+                await ctx.channel.send(file=graph_file, embed=embed)
+
+                # Delete Image
+                os.remove(graph_path)
+
+            elif args[1] == "channel":
+                
+                # Transform Data
+                grouped = df.groupby('Channel').size().reset_index(name='Count')
+
+                # Plot Chart and Save Image
+                sns.barplot(x='Channel', y='Count', data=grouped)
+                plt.savefig(filename, dpi=200)
+                plt.clf()
+
+                # Construct Embedded Message
+                embed = discord.Embed(
+                    title="Message Count Graph",
+                    description="Number of messages posted per channel",
+                    colour=POST_COLOUR
+                    )
+
+                graph_file = discord.File(graph_path, filename=filename)
+                attach_path = "attachment://" + filename
+                embed.set_image(url=attach_path)
+                embed.set_footer(text="Chart generated using Seaborn")
+
+                # Post Message
+                await ctx.channel.send(file=graph_file, embed=embed)
+
+                # Delete Image
+                os.remove(graph_path)
+        except:
+            pass
 
 
     elif args[0] == "wordcount":
+
+        df = pd.DataFrame(messages, columns=['User', 'Channel', 'Message'])
 
         try:
             # Transform Data
@@ -141,6 +232,7 @@ async def graph(ctx, *args):
             # Plot Chart and Save Image
             sns.barplot(x='User', y='Wordcount', data=grouped)
             plt.savefig(filename, dpi=200)
+            plt.clf()
 
             # Construct Embedded Message
             embed_title = "Word Count Graph ( " + str(args[1]) + " )"
@@ -149,7 +241,7 @@ async def graph(ctx, *args):
             embed = discord.Embed(
                 title=embed_title,
                 description=embed_descr,
-                colour=post_colour
+                colour=POST_COLOUR
                 )
 
             graph_file = discord.File(graph_path, filename=filename)
@@ -171,12 +263,13 @@ async def graph(ctx, *args):
             # Plot Chart and Save Image
             sns.barplot(x='User', y='Wordcount', data=grouped)
             plt.savefig(filename, dpi=200)
+            plt.clf()
 
             # Construct Embedded Message
             embed = discord.Embed(
                 title="Word Count Graph",
                 description="Number of words posted per user",
-                colour=post_colour
+                colour=POST_COLOUR
                 )
 
             graph_file = discord.File(graph_path, filename=filename)
@@ -191,12 +284,69 @@ async def graph(ctx, *args):
             os.remove(graph_path)
     
     elif args[0] == "emojicount":
-        pass
+
+        # TODO - Specific Emoji by Users
+        # TODO - Specific User Emojis Used
+
+        # Transform Data
+        emoji_list = []
+
+        for emoji in bot.guilds[0].emojis:
+            emoji_data = [emoji.name, 0] 
+            emoji_list.append(emoji_data)
+
+        found_list = []
+        for msg in messages:
+            content = msg[2]
+            found_emojis = re.findall(r'<:\w*:\d*>', content)
+            for f in found_emojis:
+                found_list.append(f)
+
+        emoji_used = []
+        for found in found_list:
+            emoji_used.append(found.split(':')[1])
+
+        for emoji in emoji_list:
+            for used in emoji_used:
+                if emoji[0] in used:
+                    emoji[1] += 1
+
+        df = pd.DataFrame(emoji_list, columns=['Emoji', 'Count'])
+
+        # Plot Chart and Save Image
+        sns.barplot(x='Emoji', y='Count', data=df)
+        plt.savefig(filename, dpi=200)
+        plt.clf()
+
+        # Construct Embedded Message
+        embed = discord.Embed(
+            title="Emoji Count Graph",
+            description="Number of emojis posted",
+            colour=POST_COLOUR
+            )
+
+        graph_file = discord.File(graph_path, filename=filename)
+        attach_path = "attachment://" + filename
+        embed.set_image(url=attach_path)
+        embed.set_footer(text="Chart generated using Seaborn")
+
+        # Post Message
+        await ctx.channel.send(file=graph_file, embed=embed)
+
+        # Delete Image
+        os.remove(graph_path)
+
 
     elif args[0] == "reactcount":
+
+        # TODO - Count of reactions used
+
         pass
 
     elif args[0] == "imagecount":
+
+        # TODO - Count of images posted
+
         pass
 
     else:
---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 Analytico_Bot.py
